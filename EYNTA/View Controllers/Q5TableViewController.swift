fa169d491a666ff61f45285426d865c95c5d2a7f
James Slusser <jslusser@kurkuma.co>
2021-04-08 10:48:13 -0400

    Fixed Ch 5

--- EYNTA/View Controllers/Q5TableViewController.swift
+++ EYNTA/View Controllers/Q5TableViewController.swift
@@ -12,8 +12,8 @@ class Q5TableViewController: UITableViewController, CellProtocol {
 
     // MARK: This question array is for the tableView data
     var questions = [Question]()
-    // MARK: This question array is for the user selected questions
-    var selectedQuestions = [Question]()
+//    // MARK: This question array is for the user selected questions
+//    var selectedQuestions = [Question]()
     
     func switchButtonTapped(WithStatus status: Bool, ForCell myCell: QuestionsTableViewCell) {
         // using guard let syntax to unwrap the optional; if it returns nil then it exits the function and does nothing
@@ -22,23 +22,51 @@ class Q5TableViewController: UITableViewController, CellProtocol {
         let ch5SwitchSelected = myCell.questionLabel.text!
         print("Question added/removed was \(String(describing: ch5SwitchSelected))")
         // MARK: - This code block adds or removes selected ingredients based on the status of the switch.
+        
+        questions[indexPath.row].isSelected = status
         if status {
-            selectedQuestions.append(questions[indexPath.row])
+            QuestionStorage.shared.saveSelectedQuestion(question: questions[indexPath.row])
         } else {
             // MARK: - This logic is here in the event that a given ingredient isn't in the selected ingredients array
-            guard let index = selectedQuestions.firstIndex(where: { $0.questionCopy == questions[indexPath.row].questionCopy }) else { return }
-            selectedQuestions.remove(at: index)
+            QuestionStorage.shared.removeSelectedQuestion(question: questions[indexPath.row])
         }
     }
     
+//    func switchButtonTapped(WithStatus status: Bool, ForCell myCell: QuestionsTableViewCell) {
+//        // using guard let syntax to unwrap the optional; if it returns nil then it exits the function and does nothing
+//        guard let indexPath = self.tableView.indexPath(for: myCell) else { return }
+//        print("cell at indexpath \(String(describing: indexPath)) tapped with switch status \(status)")
+//        let ch5SwitchSelected = myCell.questionLabel.text!
+//        print("Question added/removed was \(String(describing: ch5SwitchSelected))")
+//        // MARK: - This code block adds or removes selected ingredients based on the status of the switch.
+//        if status {
+//            selectedQuestions.append(questions[indexPath.row])
+//        } else {
+//            // MARK: - This logic is here in the event that a given ingredient isn't in the selected ingredients array
+//            guard let index = selectedQuestions.firstIndex(where: { $0.questionCopy == questions[indexPath.row].questionCopy }) else { return }
+//            selectedQuestions.remove(at: index)
+//        }
+//    }
+
+    func checkForSavedSelectedQuestions() {
+        let savedQuestions = QuestionStorage.shared.getSelectedQuestions(by: .ch5)
+        
+        savedQuestions.forEach { savedQuestion in
+            if let indexInViewControllerQuestionsArray = questions.firstIndex(where: { question in
+                question.questionCopy == savedQuestion.questionCopy
+            }) {
+                questions[indexInViewControllerQuestionsArray] = savedQuestion
+            }
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
+//        print(UserDefaults.standard.dictionaryRepresentation())
         tableView.rowHeight = UITableView.automaticDimension
         tableView.estimatedRowHeight = 600
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-        
+
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
         guard let url = Bundle.main.url(forResource: "ch5Questions", withExtension: "json"),
@@ -47,12 +75,37 @@ class Q5TableViewController: UITableViewController, CellProtocol {
                 print("Error loading JSON")
                 fatalError()
         }
-        print(ch5questions)
+
         self.questions = ch5questions
+        checkForSavedSelectedQuestions()
         let questionsNib = UINib(nibName: "QuestionsTableViewCell", bundle: nil)
         tableView.register(questionsNib, forCellReuseIdentifier: "QuestionsTableViewCell")
-        tableView.estimatedRowHeight = 50
+       // tableView.estimatedRowHeight = 50
     }
+    
+    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        tableView.rowHeight = UITableView.automaticDimension
+//        tableView.estimatedRowHeight = 600
+//        // Uncomment the following line to preserve selection between presentations
+//        // self.clearsSelectionOnViewWillAppear = false
+//
+//        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+//        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+//        guard let url = Bundle.main.url(forResource: "ch5Questions", withExtension: "json"),
+//            let ch5JSONData = try? Data(contentsOf: url),
+//            let ch5questions = try? JSONDecoder().decode(Array<Question>.self, from: ch5JSONData) else {
+//                print("Error loading JSON")
+//                fatalError()
+//        }
+//        print(ch5questions)
+//        self.questions = ch5questions
+//        checkForSavedSelectedQuestions()
+//        let questionsNib = UINib(nibName: "QuestionsTableViewCell", bundle: nil)
+//        tableView.register(questionsNib, forCellReuseIdentifier: "QuestionsTableViewCell")
+//        tableView.estimatedRowHeight = 50
+//    }
     
     // MARK: - Table view data source
     
@@ -66,14 +119,22 @@ class Q5TableViewController: UITableViewController, CellProtocol {
         return self.questions.count
     }
     
-    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "QuestionsTableViewCell", for: indexPath) as! QuestionsTableViewCell
         let row = indexPath.row
-        cell.configure(questionCopy: questions[row].questionCopy, isSelected: selectedQuestions.contains {$0.questionCopy == questions[row].questionCopy}, setDelegate: self)
-        
+        cell.configure(questionCopy: questions[row].questionCopy, isSelected: questions[row].isSelected, setDelegate: self)
+
         return cell
     }
+    
+    
+//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "QuestionsTableViewCell", for: indexPath) as! QuestionsTableViewCell
+//        let row = indexPath.row
+//        cell.configure(questionCopy: questions[row].questionCopy, isSelected: selectedQuestions.contains {$0.questionCopy == questions[row].questionCopy}, setDelegate: self)
+//        
+//        return cell
+//    }
     
     
     /*
